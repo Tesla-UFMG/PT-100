fe36f6dbf07231398122d48dd2f959a7da146c02
Leonardo Fernandes <leonardodesarocha@gmail.com>
2023-07-22 23:26:52 -0300

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "Max31865.h
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
